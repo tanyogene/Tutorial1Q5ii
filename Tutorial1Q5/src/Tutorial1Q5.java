003d1c2fb054450771a134ed5e136a4e39c238ee
Lenovo <Lenovo@DESKTOP-AF13MFN>
2020-06-16 22:24:22 +0800

    Tutorial1Q5

---
+++ Tutorial1Q5/src/Tutorial1Q5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Tutorial1Q5 {
+    
+    public static void main(String[] args) {
+        int minNum, maxNum, sum=0;
+        
+        
+        Scanner scan = new Scanner(System.in);
+       
+        System.out.print(" Enter min values: ");
+        minNum = scan.nextInt();
+        
+        System.out.print(" Enter max values: ");
+        maxNum = scan.nextInt();
+        
+        for (int i=minNum ; i<=maxNum ;i++ ){
+            
+            if (i % 2 != 0) {
+                System.out.println(i);
+                sum+= i;
+            } 
+          
+        }
+        System.out.println("Sum of all this odd number is " + sum);
+        
+        
+    }
+    
+}
